70da3002c3a059e74e0964f3388bbfbf0c08d3c3
Gerald Marechal <gerald1973@gmail.com>
2020-12-12 17:33:42 +0100

    compile_pinta.sh updated to update the mono library, from the official repository.

--- compile_pinta.sh
+++ compile_pinta.sh
@@ -2,9 +2,13 @@
 echo "********************************************************************************"
 echo "* Compile Pinta                                                                *"
 echo "********************************************************************************"
-sudo apt -y install make automake autoconf mono-devel gtk-sharp2 intltool
+sudo apt install apt-transport-https dirmngr gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian stable-raspbianbuster main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update && sudo apt upgrade
+sudo apt install make automake autoconf mono-devel gtk-sharp2 intltool
 . ./git_fetch_last_release.sh -r 'https://github.com/PintaProject/Pinta.git' -t '1.7'
-cd $FETCHING_DIRECTORY
+cd $FETCHING_DIRECTORY 
 ./autogen.sh
 if [ $? = 0 ]; then
   make
